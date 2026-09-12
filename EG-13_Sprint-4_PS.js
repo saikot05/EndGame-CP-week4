d88aa70ccdc37863618617fa1fff1509320348ff
saikot05 <saikotislam08@gmail.com>
2026-09-12 21:49:50 +0600

    Solve findDifference problem

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -28,3 +28,20 @@ var wordPattern = function(pattern, s) {
     }
     return true;
 };
+//03. Find the Difference
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let sortedS = s.split('').sort();
+    let sortedT = t.split('').sort();
+    for (let i = 0; i < sortedT.length; i++) {
+        if (sortedS[i] !== sortedT[i]) {
+            return sortedT[i];
+        }
+    }
+    return sortedT[sortedT.length - 1];
+};
+//console.log(findTheDifference("abcd", "abcde"))
