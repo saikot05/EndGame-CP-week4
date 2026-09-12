329c8451f392de25f4da409adfc669faf68936e2
saikot05 <saikotislam08@gmail.com>
2026-09-12 21:32:17 +0600

    Solve wordpattern problem

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -12,3 +12,19 @@ var isIsomorphic = function(s, t) {
     }
     return true
 };
+//02. Word Pattern
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let words = s.split(' ');
+    if (pattern.length !== words.length) return false
+    for (let i = 0; i < pattern.length; i++) {
+        if (pattern.indexOf(pattern[i]) !== words.indexOf(words[i])) {
+            return false;
+        }
+    }
+    return true;
+};
